9bf4b089fcc68662396d5721ac7c3761c2850bb6
john gascoigne <jragascoigne@gmail.com>
2025-10-22 15:11:40 +1300

    feat: Update page content

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const body = document.querySelector("#body");
 let x = 0;
 let curIndex = 0;
-let minScroll = -1;
+let minScroll = -2;
 let maxScroll = 1;
 let scrollTimeout;
+
+window.onresize = () => {
+    const vw = window.innerWidth;
+    body.style.transform = `translateX(${x * vw}px)`;
+    body.style.transition = "";
+}
 
 window.addEventListener("wheel", function (e) {
     const vw = window.innerWidth;
@@ -14,30 +20,30 @@ window.addEventListener("wheel", function (e) {
         return;
     }
 
-    const scrollMod = (1 - Math.min(1, Math.abs((x - curIndex * vw) / vw * 1.75)));
+    const scrollMod = (1 - Math.min(1, Math.abs((x - curIndex) / 0.55)));
 
     // Adjust x position based on scroll direction
-    x -= (0.01 * e.deltaY ** 2) * Math.sign(e.deltaY) * scrollMod
+    x -= ((0.01 * e.deltaY ** 2) * Math.sign(e.deltaY) * scrollMod) / vw;
 
     // Apply transform immediately
-    body.style.transform = `translateX(${x}px)`;
+    body.style.transform = `translateX(${x * vw}px)`;
 
     // Clear any existing timer
     clearTimeout(scrollTimeout);
 
     // Start a new timer — when it finishes, we assume scrolling stopped
     scrollTimeout = setTimeout(() => {
-        let index = Math.round(x / vw);
+        let index = Math.round(x);
 
         if (index < minScroll) index = minScroll;
         if (index > maxScroll) index = maxScroll;
 
-        x = index * vw;
+        x = index
         curIndex = index;
 
         // Smooth transition to snapped position
         body.style.transition = "transform 0.3s ease";
-        body.style.transform = `translateX(${x}px)`;
+        body.style.transform = `translateX(${x * vw}px)`;
 
         // Remove the transition after it completes
         setTimeout(() => {
@@ -54,12 +60,12 @@ function setIndex(newIndex) {
     if (newIndex < minScroll) newIndex = minScroll;
     if (newIndex > maxScroll) newIndex = maxScroll;
 
-    x = newIndex * vw;
+    x = newIndex;
     curIndex = newIndex;
 
     // Smooth transition to snapped position
     body.style.transition = "transform 0.8s ease";
-    body.style.transform = `translateX(${x}px)`;
+    body.style.transform = `translateX(${x * vw}px)`;
 
     // Remove the transition after it completes
     setTimeout(() => {
