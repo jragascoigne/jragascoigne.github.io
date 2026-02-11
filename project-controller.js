39c28e57d3ec15b51d90bc9a5d963b37b7563f32
slickramen <slickestramen@gmail.com>
2026-02-11 21:59:54 +1100

    feat: accordion

---
+++ project-controller.js
@@ -0,0 +1,11 @@
+const projects = document.querySelectorAll('.project');
+
+for (const project of projects) {
+    project.addEventListener('click', () => {
+        for (const op of projects) {
+            op.classList.remove('selected');
+        }
+
+        project.classList.add('selected');
+    });
+}
