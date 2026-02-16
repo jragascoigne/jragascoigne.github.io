304b83e295c0bcbf31068443cd2aa0668e00d38a
john gascoigne <jragascoigne@gmail.com>
2026-02-17 12:10:24 +1300

    feat: changed animation to requestAnimationFrame to reduce lag on windows

--- hello-handler.js
+++ hello-handler.js
@@ -3,17 +3,27 @@ const helloTitle = document.getElementById('hello-title')
 const helloWordsList = ['Hi', 'Hey', 'Hello', 'こんにちは', 'Bonjour']
 let index = 0;
 
-
 let typewriterInd = 0;
-let speed = 50;
+let lastTypeTime = 0;
+let lastUpdateTime = 0;
+const typeSpeed = 50; // ms per character
+const updateInterval = 4000; // ms between words
 
-function typeWriter() {
-    if (typewriterInd < helloWordsList[index].length) {
-        helloTitle.innerHTML += helloWordsList[index].charAt(typewriterInd);
-        typewriterInd++;
-
-        setTimeout(typeWriter, speed);
+function typeWriter(timestamp) {
+    if (timestamp - lastTypeTime >= typeSpeed) {
+        if (typewriterInd < helloWordsList[index].length) {
+            helloTitle.innerHTML += helloWordsList[index].charAt(typewriterInd);
+            typewriterInd++;
+            lastTypeTime = timestamp;
+        }
     }
+
+    if (timestamp - lastUpdateTime >= updateInterval && typewriterInd >= helloWordsList[index].length) {
+        updateHelloLabel();
+        lastUpdateTime = timestamp;
+    }
+
+    requestAnimationFrame(typeWriter);
 }
 
 const updateHelloLabel = () => {
@@ -30,8 +40,6 @@ const updateHelloLabel = () => {
 
     helloTitle.innerHTML = '';
     typewriterInd = 0;
-
-    typeWriter();
 }
 
-setInterval(updateHelloLabel, 4000);
+requestAnimationFrame(typeWriter);
